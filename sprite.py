f6d21eca74c05bcf66f89ebeaa47ead7992d4ed6
Chryson <joelthulhu@gmail.com>
2011-11-05 19:08:51 -0400

    added sprite class

---
+++ sprite.py
@@ -0,0 +1,7 @@
+class Sprite:
+    def __init__(self, image_url, width=32, height=32, x=0, y=0):
+        self.image_url = image_url
+        self.width
+        self.height
+        self.x = x
+        self.y = y
