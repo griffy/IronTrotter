9d5504b66287fc9e95df14f99c26bb2dc95b6a98
LindseyB <hazardous.narayan@gmail.com>
2011-11-06 14:05:10 -0500

    doesn't do a thing, it almost does a thing

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 from twisted.internet import reactor, protocol
+from twisted.internet.task import LoopingCall
 
 from stats import Stats
 from update import Update
+from random import choice
+from sprite import UP, DOWN, LEFT, RIGHT
 import pickle
 
 import map
@@ -73,8 +76,49 @@ def main():
     factory = MyFactory(glob)
 
     reactor.listenTCP(8000,factory)
-    reactor.run()
 
+    def move_enemy():
+        if not glob.map.layers:
+            return
+
+        for entity in glob.map.layers[2].entities:         
+            moves = []
+            if glob.map.is_player_up(entity):
+                moves.append(UP)
+            if glob.map.is_player_down(entity):
+                moves.append(DOWN)
+            if glob.map.is_player_left(entity):                         
+                moves.append(LEFT)
+            if glob.map.is_player_right(entity):
+                moves.append(RIGHT)
+            
+            if not moves:
+                move = choice([UP, DOWN, LEFT, RIGHT])
+            else:
+                move = choice(moves)
+            
+            entity.sprite.set_direction(move)
+
+            if move == UP:
+                if not glob.map.is_entity_blocked_up(entity):
+                    entity.stats.y -= 1
+            elif move == DOWN:
+                if not glob.map.is_entity_blocked_down(entity):
+                    entity.stats.y += 1    
+            elif move == LEFT:
+                if not glob.map.is_entity_blocked_left(entity):
+                    entity.stats.x -= 1
+            else:
+                if not glob.map.is_entity_blocked_right(entity):
+                    entity.stats.x += 1
+
+            # send updated postion information            
+
+    self.lc = LoopingCall(move_enemy())
+    self.lc.start(0.1)
+
+    reactor.run()
+    
 # this only runs if the module was *not* imported
 if __name__ == '__main__':
     main()
