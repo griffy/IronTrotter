33dce836498fa1147125ed24555bc1eb09dcfe69
Chryson <joelthulhu@gmail.com>
2011-11-06 14:55:45 -0500

    ghosts are killable

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from update import Update
 from random import choice
 from sprite import UP, DOWN, LEFT, RIGHT
 import pickle
+from entity import is_living
 
 import map
 import entity
@@ -61,8 +62,12 @@ class ServerGlobals:
         self.map = map.generate_map(map.map_width,map.map_height)
 
     def update(self, up):
-        if up.name != "":
-            self.map.addPlayer(up)
+        if isinstance(up, Update):
+            if is_living(up.enttype) and up.stats.hp <= 0:
+                self.map.layers[2].entities.remove(self.map.layers[2].getById(up.idnum))
+        else:
+            if up.name != "":
+                self.map.addPlayer(up)
 
 def main():
     """This runs the protocol on port 8000"""
@@ -81,30 +86,28 @@ def main():
         if not glob.map.layers:
             return
 
-        for entity in glob.map.layers[2].entities:         
+        for entity in glob.map.layers[2].entities:
             moves = []
             if glob.map.is_player_up(entity):
                 moves.append(UP)
             if glob.map.is_player_down(entity):
                 moves.append(DOWN)
-            if glob.map.is_player_left(entity):                         
+            if glob.map.is_player_left(entity):
                 moves.append(LEFT)
             if glob.map.is_player_right(entity):
                 moves.append(RIGHT)
-            
+
             if not moves:
                 move = choice([UP, DOWN, LEFT, RIGHT])
             else:
                 move = choice(moves)
-            
-            entity.sprite.set_direction(move)
 
             if move == UP:
                 if not glob.map.is_entity_blocked_up(entity):
                     entity.stats.y -= 1
             elif move == DOWN:
                 if not glob.map.is_entity_blocked_down(entity):
-                    entity.stats.y += 1    
+                    entity.stats.y += 1
             elif move == LEFT:
                 if not glob.map.is_entity_blocked_left(entity):
                     entity.stats.x -= 1
@@ -112,13 +115,15 @@ def main():
                 if not glob.map.is_entity_blocked_right(entity):
                     entity.stats.x += 1
 
-            # send updated postion information            
+            # send updated position information to all other clients
+            for t in factory.transports:
+                t.write(pickle.dumps(entity.getUpdate(),2))
 
-    self.lc = LoopingCall(move_enemy())
-    self.lc.start(0.1)
+    lc = LoopingCall(move_enemy)
+    lc.start(1)
 
     reactor.run()
-    
+
 # this only runs if the module was *not* imported
 if __name__ == '__main__':
     main()
