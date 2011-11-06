21b962766e4a0cac6c379761926b0d3c3ea1d1e8
wolfwood <wolfwood@xomb.org>
2011-11-06 02:00:33 -0500

    handler swapping

--- handler.py
+++ handler.py
@@ -25,8 +25,11 @@ class Handler:
         self.f = protocol.ClientFactory()
         self.f.protocol = TrotterSub
 
+        self.fontDrawer = font.Font("font/youmurdererbb_reg.ttf", 100, colors.RED)
+
         self.counter = 5
         self.drawText = False
+
         pygame.mixer.init()
         self.titleMusic = sound.Sound("music/severedfifth_endofdays.ogg")
         self.titleMusic.play()
@@ -47,10 +50,12 @@ class Handler:
                 elif event.key == pygame.K_RETURN:
                     self.titleMusic.stop()
                     print "DO A THING"
+                    self.lc.stop()
+                    self.lc = LoopingCall(self.lobbyevent)
+                    self.lc.start(0.1)
+
                 elif event.key == pygame.K_x:
                     pygame.event.post(pygame.event.Event(pygame.QUIT))
-
-        fontDrawer = font.Font("font/youmurdererbb_reg.ttf", 100, colors.RED)
 
         self.screen.blit(self.title, self.titlerect)
         if self.counter == 0:
@@ -59,6 +64,32 @@ class Handler:
         self.counter -= 1
 
         if self.drawText:
-            fontDrawer.draw(400,450, "PRESS ENTER TO START")
+            self.fontDrawer.draw(400,450, "PRESS ENTER TO START")
 
         pygame.display.flip()
+
+    def lobbyevent(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                reactor.stop()
+            elif event.type == pygame.KEYDOWN:
+                # --- KEY handlers go HERE ---
+                if event.key == pygame.K_RETURN:
+                    self.lc.stop()
+                    self.lc = LoopingCall(self.gameevent)
+                    self.lc.start(0.1)
+
+                elif event.key == pygame.K_x:
+                    pygame.event.post(pygame.event.Event(pygame.QUIT))
+
+        self.screen.blit(self.title, self.titlerect)
+        if self.counter == 0:
+            self.drawText = not self.drawText
+            self.counter = 5
+        self.counter -= 1
+
+        if self.drawText:
+            self.fontDrawer.draw(400,450, "PRESS ENTER TO START")
+
+        pygame.display.flip()
+
